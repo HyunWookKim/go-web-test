68bf41cc4b9fc13bd091bf622c1551d17c2217bb
Hyunwook <gusdnr05@gmail.com>
2021-09-12 22:01:08 +0900

    Go Web Test Code

---
+++ WEB5/myapp/app_test.go
@@ -0,0 +1,42 @@
+package myapp
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndesx(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TestUsers(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/users")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	assert.Contains(string(data), "Get UserInfo")
+	// assert.Equal("Hello World", string(data))
+}
